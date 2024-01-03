8b7a14ba31b0c504d232d4529007693908c4bf4c
Faris Siddiqi <farisiddiqi@gmail.com>
2024-01-02 21:14:24 -0600

    updated notifications

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ GPIO.setup(SENSOR_PIN, GPIO.IN)  # Set the sensor pin as input
 try:
     while True:
         num = GPIO.input(SENSOR_PIN)
-        print(num)
-
+        if num == 1:
+            print('water detected')
+        else:
+            print('no water')
 except KeyboardInterrupt:
     GPIO.cleanup()
