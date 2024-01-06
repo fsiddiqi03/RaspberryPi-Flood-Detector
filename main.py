3d47948a84cdb92030a98b044945fd0f86d7733c
Faris Siddiqi <farisiddiqi@gmail.com>
2024-01-05 20:47:08 -0600

    changed the logic for changing the light color

--- main.py
+++ main.py
@@ -3,25 +3,42 @@ import RPi.GPIO as GPIO
 import time
 
 
-hue = hueAPI()
+def main(): 
+    # create the hueAPI object 
+    hue = hueAPI()
 
 
-# GPIO pin number where the sensor is connected
-SENSOR_PIN = 18 
+    # GPIO pin number where the sensor is connected
+    SENSOR_PIN = 18 
 
-GPIO.setmode(GPIO.BCM)  # BCM pin numbering
-GPIO.setup(SENSOR_PIN, GPIO.IN)  # Set the sensor pin as input
+    GPIO.setmode(GPIO.BCM)  # BCM pin numbering
+    GPIO.setup(SENSOR_PIN, GPIO.IN)  # Set the sensor pin as input
 
-try:
-    while True:
-        num = GPIO.input(SENSOR_PIN)
-        if num == 1:
-            print('water detected')
-            # use hue api to change light color to blue
-            hue.changeColor(0.15, .20)
-        else:
-            print('no water')
-            hue.changeColor(0.39, 0.33)
-except KeyboardInterrupt:
-    GPIO.cleanup()
 
+    light_changed = False
+
+    try:
+        while True:
+            num = GPIO.input(SENSOR_PIN)
+            # 1 means water is deteced 
+            if num == 1:
+                print('water detected')
+                if not light_changed:
+                    # use hue api to change light color to blue
+                    hue.changeColor(0.15, .20)
+                    light_changed = True
+            # checks if the GPIO sends back 0, meaning no water 
+            else:
+                print('no water')
+                # if the light was changed, meaning its blue, return it back to its normal state 
+                if light_changed:
+                    hue.changeColor(0.39, 0.33)
+                    light_changed = False
+            time.sleep(0.25)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+
+
+
+if __name__ == '__main__':
+    main()
